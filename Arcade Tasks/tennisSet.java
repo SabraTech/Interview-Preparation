6ccb13ee1cd5d4f581d8c26fbfa227f0ac3429c3
sabra <mohamed2006fs@gmail.com>
2017-03-31 19:51:19 +0200

    Solved new tasks

--- Arcade Tasks/tennisSet.java
+++ Arcade Tasks/tennisSet.java
@@ -1,6 +1,11 @@
 boolean tennisSet(int score1, int score2) {
-    if (score1 == 6 && score2 < 5 || score2 == 6 && score1 < 5 || score1 == 7 && score2 < 6 || score2 == 7 && score1 < 6){
+    if ((score1 >= 8 || score2 >= 8) || (score1 == score2)){
+        return false;
+    } else if ((score1 == 6 && score2 < 5) || (score2 == 6 && score1 < 5)){
         return true;
+    } else if ((score1 == 7 && score2 >= 5) || (score2 == 7 && score1 >= 5)){
+        return true;
+    }else{
+        return false;
     }
-    return false;
 }
