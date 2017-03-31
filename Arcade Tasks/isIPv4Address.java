dc186138e85c060bc49c9225747d1d0d86828c8e
sabra <mohamed2006fs@gmail.com>
2017-03-31 19:26:32 +0200

    Solved new tasks

---
+++ Arcade Tasks/isIPv4Address.java
@@ -0,0 +1,5 @@
+boolean isIPv4Address(String inputString) {
+    final Pattern PATTERN = Pattern.compile(
+        "^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+    return PATTERN.matcher(inputString).matches();
+}
---
+++ Arcade Tasks/tennisSet.java
@@ -0,0 +1,6 @@
+boolean tennisSet(int score1, int score2) {
+    if (score1 == 6 && score2 < 5 || score2 == 6 && score1 < 5 || score1 == 7 && score2 < 6 || score2 == 7 && score1 < 6){
+        return true;
+    }
+    return false;
+}
