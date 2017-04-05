81106b5efc94dc54f5c78a0a72a6548549c10c4e
sabra <mohamed2006fs@gmail.com>
2017-04-05 20:53:03 +0200

    Solved new tasks

--- Arcade Tasks/removeArrayPart.java
+++ Arcade Tasks/removeArrayPart.java
@@ -1,10 +1,10 @@
 int[] removeArrayPart(int[] inputArray, int l, int r) {
     int[] ans = new int[inputArray.length - (r + 1 - l)];
     int k = 0;
-    for(int i = 0; i < l; i++){
-        ans[k++] = inputArray[i];
-    }
-    for(int i = r + 1; i < inputArray.length; i++){
+    for(int i = 0; i < inputArray.length; i++){
+        if(i >= l && i <= r){
+            continue;
+        }
         ans[k++] = inputArray[i];
     }
     return ans;
