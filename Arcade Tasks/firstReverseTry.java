ebf78984837ebfc8e03a9c956146e001d41075a0
sabra <mohamed2006fs@gmail.com>
2017-04-05 20:43:32 +0200

    Solved new tasks

---
+++ Arcade Tasks/firstReverseTry.java
@@ -0,0 +1,9 @@
+int[] firstReverseTry(int[] arr) {
+    if(arr.length == 0){
+        return arr;
+    }
+    int temp = arr[0];
+    arr[0] = arr[arr.length - 1];
+    arr[arr.length - 1] = temp;
+    return arr;
+}
