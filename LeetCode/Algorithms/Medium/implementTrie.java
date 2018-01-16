8fa815c7ab6a2e252d6a1725f8dee14c85cf57a3
sabra <mohamed2006fs@gmail.com>
2018-01-16 18:32:09 +0200

    Adding leetcode problems

---
+++ LeetCode/Algorithms/Medium/implementTrie.java
@@ -0,0 +1,82 @@
+class TrieNode {
+    // Initialize your data structure here.
+    private TrieNode[] links;
+    private final int R = 26;
+    private boolean isEnd;
+
+    public TrieNode(){
+        links = new TrieNode[R];
+    }
+
+    public boolean containsKey(char ch){
+        return links[ch-'a'] != null;
+    }
+
+    public TrieNode get(char c){
+        return links[c-'a'];
+    }
+
+    public void put(char ch, TrieNode node){
+        links[ch-'a'] = node;
+    }
+
+    public void setEnd(){
+        isEnd = true;
+    }
+
+    public boolean isEnd(){
+        return isEnd;
+    }
+}
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    // Inserts a word into the trie.
+    public void insert(String word) {
+        TrieNode node = root;
+        for(int i=0;i<word.length();i++){
+            char currentChar = word.charAt(i);
+            if(!node.containsKey(currentChar)){
+                node.put(currentChar, new TrieNode());
+            }
+            node = node.get(currentChar);
+        }
+        node.setEnd();
+    }
+
+    // Returns if the word is in the trie.
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
+    }
+
+    private TrieNode searchPrefix(String word){
+        TrieNode node = root;
+        for(int i=0;i<word.length();i++){
+            char currentChar = word.charAt(i);
+            if(node.containsKey(currentChar)){
+                node = node.get(currentChar);
+            }else{
+                return null;
+            }
+        }
+        return node;
+    }
+
+    // Returns if there is any word in the trie
+    // that starts with the given prefix.
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+}
+
+// Your Trie object will be instantiated and called as such:
+// Trie trie = new Trie();
+// trie.insert("somestring");
+// trie.search("key");
---
+++ LeetCode/Algorithms/Medium/oddEvenList.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+    if (head == null) return null;
+    ListNode odd = head, even = head.next, evenHead = even;
+    while (even != null && even.next != null) {
+        odd.next = even.next;
+        odd = odd.next;
+        even.next = odd.next;
+        even = even.next;
+    }
+    odd.next = evenHead;
+    return head;
+}
+}
